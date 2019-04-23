f69544e10937072ce65c133efdd83f6d9d619859
benjaminfff <990460889@qq.com>
2019-04-23 17:01:02 +0800

    play puzzle func succeed

--- src/components/puzzlelearn/play-puzzle.vue
+++ src/components/puzzlelearn/play-puzzle.vue
@@ -1,20 +1,44 @@
 <template>
   <div class="play-puzzle">
-    <div class="left-container"></div>
+    <div class="left-container">
+      <el-scrollbar class="m-scrollbar">
+        <div v-for="card in vCards"
+             style="width: 80%;margin-left: 10%;user-select: none;cursor: pointer;margin-bottom: 2rem"
+             @click="activeCells(card.index,orientation.VERTICAL)">
+          <div
+            style="background-color: white;padding: 2rem;margin: 10px;border-radius: 10px;min-height: 10rem;display: flex;justify-content: center;align-items: center;color: gray">
+            <div style="font-size: 1.2rem;">{{card.definition}}</div>
+            <div :class="{'is-active':card.isActive}">{{card.index}}</div>
+          </div>
+        </div>
+      </el-scrollbar>
+    </div>
     <div class="middle-container">
       <div class="createPuzzle__cells" :style="{'--row':Math.sqrt(size),'--column':Math.sqrt(size)}">
         <div class="cell-container" v-for="cell in cells" style="margin: 3px">
           <div class="cp-cell" style="width: 100%;height: 100%"
                :class="{'is-active':cell.isActive,'is-invisible':cell.invisible}" @click="cellClick(cell)">
             <el-input maxlength="1" class="cp-cell__input" v-on:focus="inputFocus($event,cell)" v-model="cell.c"
-                      ref="cells" @input="inputChange(cell)"></el-input>
-            <div class="cp-cell__h">{{cell.h!=0?cell.h:""}}</div>
-            <div class="cp-cell__v">{{cell.v!=0?cell.v:""}}</div>
+                      ref="cells" @input="inputChange(cell)" :disabled="cell.answerCorrect"></el-input>
+            <div class="cp-cell__h">{{cell.h!=0&&cell.isHhead?cell.h:""}}</div>
+            <div class="cp-cell__v">{{cell.v!=0&&cell.isVhead?cell.v:""}}</div>
           </div>
         </div>
       </div>
     </div>
-    <div class="right-container"></div>
+    <div class="right-container">
+      <el-scrollbar class="m-scrollbar">
+        <div v-for="card in hCards"
+             style="width: 80%;margin-left: 10%;user-select: none;cursor: pointer;margin-bottom: 2rem;color: gray"
+             @click="activeCells(card.index,orientation.HORIZONTAL)">
+          <div
+            style="background-color: white;padding: 2rem;margin: 10px;border-radius: 10px;min-height: 10rem;display: flex;justify-content: center;align-items: center">
+            <div style="font-size: 1.2rem;">{{card.definition}}</div>
+            <div :class="{'is-active':card.isActive}">{{card.index}}</div>
+          </div>
+        </div>
+      </el-scrollbar>
+    </div>
   </div>
 </template>
 
@@ -29,6 +53,7 @@
         puzzleInfo: {},
         lastCellClick: {},
         orientation: {},
+        cards: [],
         vCards: [],               //vertical cards
         hCards: [],                //horizontal cards
       }
@@ -53,10 +78,14 @@
             v: 0,     //vertical mark
             p: cells.length,
             isActive: false,
-            invisible: true
+            invisible: true,
+            isHhead: false,
+            isVhead: false,
+            answerCorrect: false        //当这个术语成功
           });
         }
         let chvps = puzzleData.split(",");
+        let hHeadIndices = [], vHeadIndices = [];
         chvps.forEach((chvp) => {
           let c = chvp.length == 7 ? chvp.substring(0, 1) : chvp.substring(0, 2);
           let hvp = chvp.length == 7 ? chvp.substring(1, 7) : chvp.substring(2, 8);
@@ -67,14 +96,31 @@
           cells[p].h = h;
           cells[p].v = v;
           cells[p].invisible = false;
+          !hHeadIndices.includes(h) && h != 0 ? (cells[p].isHhead = true, hHeadIndices.push(h)) : null;
+          !vHeadIndices.includes(v) && v != 0 ? (cells[p].isVhead = true, vHeadIndices.push(v)) : null;
         })
         this.cells = cells;
-        console.log(this.cells);
       },
       initCards() {
-        this.cells.forEach((cell) => {
-
+        let vIndices = [], hIndices = [];
+        let cards = this.vocabularies.sort((c1, c2) => {
+          return c1.vid - c2.vid
         });
+        cards.forEach((card, index) => {
+          card.index = index + 1;
+          card.isActive = false;
+        });
+        this.cells.forEach((cell) => {
+          if (!cell.invisible) {
+            !vIndices.includes(cell.v) && cell.v != 0 ? vIndices.push(cell.v) : null;
+            !hIndices.includes(cell.h) && cell.h != 0 ? hIndices.push(cell.h) : null;
+          }
+        });
+        cards.forEach((card) => {
+          vIndices.includes(card.index) ? this.vCards.push(card) : null;
+          hIndices.includes(card.index) ? this.hCards.push(card) : null;
+        });
+        this.cards = cards;
       },
       async fetchData() {
         let sid = this.$route.params.sid;
@@ -96,66 +142,58 @@
           }
         }).then((res) => {
           this.vocabularies = res.data.vocabularies;
-          console.log(res.data);
         });
-      },
-      getFarthestPos(cell) {             //get the cell's farthest left, right, top, bottom.
-        let rowSize = Math.sqrt(this.size);
-        let pos_of_row = Math.floor(cell.p / rowSize);
-        let pos_of_col = cell.p % rowSize;
-        let left, right, top, bottom;
-        left = right = top = bottom = cell.p;
-        let fl = pos_of_row * rowSize, fr = rowSize * (pos_of_row + 1), ft = pos_of_col,
-          fb = (rowSize * rowSize - 1) + pos_of_col;
-        while (left >= fl && this.cells[left].c != "") {
-          left--;
-        }
-        while (right < fr && this.cells[right].c != "") {
-          right++;
-        }
-        while (top >= ft && this.cells[top].c != "") {
-          top -= rowSize;
-        }
-        while (bottom <= fb && this.cells[bottom].c != "") {
-          bottom += rowSize;
-        }
-        return {top, bottom, left, right};
+        this.initCards();
       },
       cellClick(cell) {
         let rowSize = Math.sqrt(this.size);
         if (cell.p == this.lastCellClick.p && cell.h != 0 && cell.v != 0) {
           let isHorizontalActive = (cell.p % rowSize != 0 && this.cells[cell.p - 1].isActive) || ((cell.p + 1) % rowSize != 0 && this.cells[cell.p + 1].isActive);
-          this.activeCells(cell, isHorizontalActive == true ? this.orientation.VERTICAL : this.orientation.HORIZONTAL);
+          this.activeCells(isHorizontalActive ? cell.v : cell.h, isHorizontalActive ? this.orientation.VERTICAL : this.orientation.HORIZONTAL);
         } else {
           let hasHorizontal = (cell.p % rowSize != 0 && this.cells[cell.p - 1].h != 0) || ((cell.p + 1) % rowSize != 0 && this.cells[cell.p + 1].h != 0);
-          this.activeCells(cell, hasHorizontal == false ? this.orientation.VERTICAL : this.orientation.HORIZONTAL);
+          this.activeCells(hasHorizontal ? cell.h : cell.v, !hasHorizontal ? this.orientation.VERTICAL : this.orientation.HORIZONTAL);
         }
         this.lastCellClick.p = cell.p;
       },
-      activeCells(cell, orientation) {              //cells near focused and filled cell can be active
+      activeCells(activeIndex, orientation) {
         let rowSize = Math.sqrt(this.size);
+        this.cards.forEach((card) => {
+          card.index == activeIndex ? card.isActive = true : card.isActive = false;
+        })
         if (orientation == this.orientation.VERTICAL) {
           this.cells.forEach((mCell) => {
-            if (!cell.invisible) {
-              mCell.v == cell.v ? mCell.isActive = true : mCell.isActive = false;
+            if (!mCell.invisible) {
+              mCell.v == activeIndex ? mCell.isActive = true : mCell.isActive = false;
             }
           });
         } else {
           this.cells.forEach((mCell) => {
-            if (!cell.invisible) {
-              mCell.h == cell.h ? mCell.isActive = true : mCell.isActive = false;
+            if (!mCell.invisible) {
+              mCell.h == activeIndex ? mCell.isActive = true : mCell.isActive = false;
             }
           });
         }
       },
       inputFocus(event, cell) {
-        this.curCell = cell;
         if (this.isKeyBoardFocus) {
           this.cellClick(cell);
           this.isKeyBoardFocus = false;
         }
         this.$nextTick(() => {
           this.$refs.cells[cell.p].select();
+        });
+      },
+      inputChange(cell) {
+        this.$refs.cells[cell.p].select();
+        let answer = "";
+        let correctAnswer = this.cards.find((card) => card.isActive).term;
+        for (let cell of this.cells) {
+          cell.isActive ? answer += cell.c : null;
+        }
+        this.cells.forEach((cell) => {
+          cell.isActive && answer == correctAnswer ? cell.answerCorrect = true : null;
+          console.log(cell.answerCorrect);
         });
       }
     }
@@ -176,18 +214,25 @@
   }
 
   .left-container {
-    flex: 1 0 0;
+    flex: 2 0 0;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 
   .middle-container {
-    flex: 8 0 0;
+    flex: 6 0 0;
     display: flex;
     justify-content: center;
     align-items: center;
   }
 
   .right-container {
-    flex: 1 0 0;
+    flex: 2 0 0;
+    height: 100%;
+    display: flex;
+    align-items: center;
   }
 
   .cell-container {
@@ -196,8 +241,48 @@
     position: relative;
   }
 
+  .is-active {
+    color: black;
+  }
+
 
   .is-invisible {
     display: none;
+  }
+
+  .hcards {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .hcard {
+    width: 100%;
+    min-height: 12rem;
+    user-select: none;
+    cursor: pointer;
+    position: relative;
+  }
+
+  .hcard-inner {
+    width: 100%;
+    height: 100%;
+    background-color: white;
+    padding: 2rem;
+    margin: 10px;
+    border-radius: 10px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+    color: gray;
+  }
+</style>
+
+<style>
+  .m-scrollbar {
+    width: 90%;
+    height: 80%;
+  }
+
+  .m-scrollbar .el-scrollbar__wrap {
+    overflow-x: hidden;
   }
 </style>
